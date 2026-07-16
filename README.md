0b91ebed527c0fa71dbfdf3433b9d597c73a8aca
Marcelo Shines <marce123567@gmail.com>
2026-07-16 16:06:48 -0300

    Commit inicial

--- README.md
+++
@@ -1,2 +0,0 @@
-# anthropic_api_experiments
-Pruebas y experimentos correspondientes al curso "Building with the Claude API"
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from working-with-anthropic-api!")
+
+
+if __name__ == "__main__":
+    main()
